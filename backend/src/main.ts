f2b98347b2a7e1fb991469527cdbb6c10015f116
vimal prakash <58904955+vimalprakash404@users.noreply.github.com>
2025-10-02 09:11:17 +0530

    Updated main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionsFilter } from './common/filters/http-exception.filter';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -17,6 +19,12 @@ async function bootstrap() {
   app.useGlobalFilters(new AllExceptionsFilter());
 
   app.enableCors();
+
+  app.useStaticAssets(join(__dirname, '..', '..', 'frontend', 'dist'), {
+    prefix: '/',
+  });
+
+  app.setBaseViewsDir(join(__dirname, '..', '..', 'frontend', 'dist'));
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
